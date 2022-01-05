84ac00eb6cef6d723c2ab44c70b62d181e76d623
CassiaFa <92436234+CassiaFa@users.noreply.github.com>
2022-01-05 10:38:41 +0100

    Création notebook
    Création du jupyter notebook.
    
    Chargement de la base de données crédit immo

--- analyse.py
+++ analyse.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+
+cd_immo = pd.read_csv("./base_donnees/credit_immo/credit_immo.csv")
+
+print(cd_immo.head())
